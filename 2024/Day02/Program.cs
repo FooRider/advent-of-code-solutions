352f86d6fe9767250b6787a607719932ceaa7710
Peter Pokojný <peter.pokojny@innovatrics.com>
2024-12-02 10:07:31 +0100

    Day02 refactoring and star2

--- 2024/Day02/Program.cs
+++ 2024/Day02/Program.cs
@@ -1,37 +1,56 @@
-﻿var inputFile = "input1.txt";
+﻿using System.Collections.Immutable;
+
+var inputFile = "input1.txt";
 
 await using var fh = File.OpenRead(inputFile);
 using var sr = new StreamReader(fh);
 
-int safeCount = 0;
+var reports = new List<ImmutableList<int>>();
 while (await sr.ReadLineAsync() is { } line)
 {
-    bool isUnsafe = false;
+    var levels = new List<int>();
     var ls = line.AsSpan();
     var enumerator = ls.Split(' ');
-    if (!enumerator.MoveNext()) continue;
-    var prev1 = int.Parse(ls[enumerator.Current]);
-    if (!enumerator.MoveNext()) continue;
-    var prev0 = int.Parse(ls[enumerator.Current]);
-    
-    var direction = Math.Sign(prev0 - prev1);
-    if (Math.Abs(prev0 - prev1) is < 1 or > 3)
-    {
-        isUnsafe = true;
-    }
-    
-    while (!isUnsafe && enumerator.MoveNext())
-    {
-        prev1 = prev0;
-        prev0 = int.Parse(ls[enumerator.Current]);
-        if (Math.Abs(prev0 - prev1) is < 1 or > 3 || Math.Sign(prev0 - prev1) != direction)
-        {
-            isUnsafe = true;
-        };
-    }
-    
-    if (!isUnsafe)
-        safeCount++;
+    while (enumerator.MoveNext())
+        levels.Add(int.Parse(ls[enumerator.Current]));
+    reports.Add(levels.ToImmutableList());
 }
 
-Console.WriteLine(safeCount);
+var reportsByAbsoluteSafety = reports
+    .GroupBy(IsAbsolutelySafe)
+    .ToDictionary(g => g.Key);
+
+{
+    Console.WriteLine("Part 1");
+    Console.WriteLine(reportsByAbsoluteSafety[true].Count());
+}
+
+{
+    Console.WriteLine("Part 2");
+    var safeOnlyWithDampening = reportsByAbsoluteSafety[false].Count(r => GetLevelsWithOneSkipped(r).Any(IsAbsolutelySafe));
+    Console.WriteLine(reportsByAbsoluteSafety[true].Count() + safeOnlyWithDampening);
+}
+
+bool IsAbsolutelySafe(IEnumerable<int> report) =>
+    !report.Aggregate((null, false, null),
+        ((int? Direction, bool UnsafeAlready, int? PreviousLevel) acc, int level) =>
+            acc switch
+            {
+                (_, true, _) => (acc.Direction, true, level),
+                (null, _, null) => (null, false, level),
+                (null, _, { } prev) => (
+                    Math.Sign(level - prev), 
+                    (Math.Abs(level - prev) is < 1 or > 3), 
+                    level),
+                ({ } dir, _, { } prev) => (
+                    dir,
+                    (Math.Abs(level - prev) is < 1 or > 3) || Math.Sign(level - prev) != dir,
+                    level)
+            }
+    ).UnsafeAlready;
+
+IEnumerable<IEnumerable<int>> GetLevelsWithOneSkipped(IReadOnlyCollection<int> inputReport)
+{
+    for (int i = 0; i < inputReport.Count; i++)
+        yield return inputReport.Take(i).Concat(inputReport.Skip(i + 1));
+}
