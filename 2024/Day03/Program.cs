9209a6258c5f3539790f74a59a6e80176c058e2d
Peter Pokojný <peter.pokojny@innovatrics.com>
2024-12-03 07:44:18 +0100

    Solution for Day 3

---
+++ 2024/Day03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+var input = await File.ReadAllTextAsync("input1.txt");
+
+{
+    Console.WriteLine("Part 1");
+    var matches = Regex.Matches(input, @"mul\(([0-9]{1,3}),([0-9]{1,3})\)");
+    Console.WriteLine(matches.Sum(m => long.Parse(m.Groups[1].Value) * long.Parse(m.Groups[2].Value)));
+}
+
+{
+    Console.WriteLine("Part 2");
+    var matches = Regex.Matches(input, @"((?<op>mul)\((?<operand1>[0-9]{1,3}),(?<operand2>[0-9]{1,3})\))|((?<op>do)\(\))|((?<op>don\'t)\(\))");
+    var ops = matches.Select(m => (m.Groups["op"].Value, m.Groups["operand1"].Value, m.Groups["operand2"].Value));
+    var enabled = true;
+    long accumulator = 0;
+    foreach (var (op, operand1, operand2) in ops)
+    {
+        if (op == "don't")
+            enabled = false;
+        if (op == "do")
+            enabled = true;
+        if (op == "mul" && enabled)
+            accumulator += long.Parse(operand1) * long.Parse(operand2);
+    }
+    Console.WriteLine(accumulator);
+}
