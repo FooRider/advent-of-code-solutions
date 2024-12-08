31479b5b1a376178950e334a2689fa68e4acd1ef
Peter Pokojný <peter.pokojny@innovatrics.com>
2024-12-08 07:57:59 +0100

    Day 8 part 2

--- 2024/Day08/Program.cs
+++ 2024/Day08/Program.cs
@@ -27,8 +27,8 @@ int height = y;
     {
         foreach (var (a1, a2) in GeneratePairs(ants))
         {
-            Point a12 = (a2.X - a1.X, a2.Y - a1.Y);
-            List<Point> ans = [(a2.X + a12.X, a2.Y + a12.Y), (a1.X - a12.X, a1.Y - a12.Y)];
+            Point vectorA12 = (a2.X - a1.X, a2.Y - a1.Y);
+            List<Point> ans = [(a2.X + vectorA12.X, a2.Y + vectorA12.Y), (a1.X - vectorA12.X, a1.Y - vectorA12.Y)];
             foreach (var point in ans
                          .Where(p => p.X >= 0 && p.Y >= 0 && p.X < width && p.Y < height)
                          .Where(p => !antinodes.Contains(p)))
@@ -36,13 +36,54 @@ int height = y;
         }
     }
     Console.WriteLine(antinodes.Count);
+}
 
-    IEnumerable<(T, T)> GeneratePairs<T>(List<T> input)
+{
+    Console.WriteLine("Part 2");
+    List<Point> antinodes = [];
+    foreach (var (freq, ants) in antennae)
     {
-        for (var i = 0; i < input.Count - 1; i++)
-        for (var j = i + 1; j < input.Count; j++)
-            yield return (input[i], input[j]);
+        foreach (var (a1, a2) in GeneratePairs(ants))
+        {
+            Point vectorA12 = Normalize((a2.X - a1.X, a2.Y - a1.Y));
+            var ans = EnumerateToInfinity()
+                .Select<int, Point>(x => (a1.X + vectorA12.X * x, a1.Y + vectorA12.Y * x))
+                .TakeWhile(p => p.X >= 0 && p.Y >= 0 && p.X < width && p.Y < height)
+                .Concat(EnumerateToInfinity()
+                    .Select<int, Point>(x => (a1.X - vectorA12.X * x, a1.Y - vectorA12.Y * x))
+                    .TakeWhile(p => p.X >= 0 && p.Y >= 0 && p.X < width && p.Y < height));
+            
+            foreach (var point in ans.Where(p => !antinodes.Contains(p)))
+                antinodes.Add(point);
+        }
     }
+    Console.WriteLine(antinodes.Count);
+
+    Point Normalize(Point vector)
+    {
+        var gcd = GCD(Math.Abs(vector.X), Math.Abs(vector.Y));
+        return new Point(vector.X / gcd, vector.Y / gcd);
+
+        int GCD(int x, int y)
+        {
+            if (y > x) return GCD(y, x);
+            if (x == y || x % y == 0) return y;
+            return GCD(y, x % y);
+        }
+    }
+
+    IEnumerable<int> EnumerateToInfinity()
+    {
+        int i = 0;
+        while (true) yield return i++;
+    }
+}
+
+IEnumerable<(T, T)> GeneratePairs<T>(List<T> input)
+{
+    for (var i = 0; i < input.Count - 1; i++)
+    for (var j = i + 1; j < input.Count; j++)
+        yield return (input[i], input[j]);
 }
 
 readonly record struct Antenna(int X, int Y, char Frequency)
