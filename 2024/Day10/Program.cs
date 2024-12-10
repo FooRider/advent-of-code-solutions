b3f0eec282df80b3986ed79875bb51aae496def6
Peter Pokojný <peter.pokojny@innovatrics.com>
2024-12-10 15:56:10 +0100

    Prepare for day10

---
+++ 2024/Day10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello World!");
+
+record Node<TN, TE>(TN Value)
+{
+    public List<Edge<TN, TE>> OutgoingEdges { get; } = [];
+    public List<Edge<TN, TE>> IncomingEdges { get; } = [];
+    
+    public void LinkTargetNode(TE value, Node<TN, TE> target)
+    {
+        var e = new Edge<TN, TE>(value, this, target);
+        OutgoingEdges.Add(e);
+        target.IncomingEdges.Add(e);
+    }
+}
+
+record Edge<TN, TE>(TE Value, Node<TN, TE> Source, Node<TN, TE> Target);
