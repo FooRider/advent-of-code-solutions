ddf91b64b37daa4c14cdfaf6dbc32da34748d29b
Peter Pokojný <peter.pokojny@innovatrics.com>
2024-12-11 07:11:41 +0100

    Day 10

---
+++ 2024/Common/Graphs.cs
@@ -0,0 +1,69 @@
+﻿namespace Common.Graphs;
+public record Node<TN, TE>(TN Value)
+{
+    public List<Edge<TN, TE>> OutgoingEdges { get; } = [];
+    public List<Edge<TN, TE>> IncomingEdges { get; } = [];
+    
+    public void LinkTargetNode(TE value, Node<TN, TE> target)
+    {
+        var e = new Edge<TN, TE>(value, this, target);
+        OutgoingEdges.Add(e);
+        target.IncomingEdges.Add(e);
+    }
+}
+
+public record Edge<TN, TE>(TE Value, Node<TN, TE> Source, Node<TN, TE> Target);
+
+public static class GraphSearch
+{
+    public static IReadOnlyCollection<(Node<TN, TE> Node, int Rank)> BreadthFirstSearch<TN, TE>(
+        Node<TN, TE> startNode,
+        Predicate<(Node<TN, TE> Node, int Rank)> resultPredicate)
+    {
+        List<(Node<TN, TE> Node, int Rank)> visitedNodes = [(startNode, 0)];
+        
+        var edgesToTraverse = new Queue<(Edge<TN, TE>, int Rank)>(
+            startNode.OutgoingEdges.Select(e => (e, 1)));
+
+        while (edgesToTraverse.TryDequeue(out var edgeT))
+        {
+            var (edge, nextNodeRank) = edgeT;
+            if (visitedNodes.Any(vn => vn.Node == edge.Target))
+                continue;
+            visitedNodes.Add((edge.Target, nextNodeRank));
+            foreach (var nextEdge in edge.Target.OutgoingEdges)
+                edgesToTraverse.Enqueue((nextEdge, nextNodeRank + 1));
+        }
+        
+        var result = visitedNodes.Where(vn => resultPredicate((vn.Node, vn.Rank))).ToList();
+        return result;
+    }
+
+    public static IReadOnlyCollection<IReadOnlyCollection<(Node<TN, TE> Node, int Rank)>> DepthFirstSearch<TN, TE>(
+        Node<TN, TE> startNode,
+        Predicate<(Node<TN, TE> Node, int Rank)> targetPredicate)
+        => DepthFirstSearch([(startNode, 0)], targetPredicate);
+    
+    private static IReadOnlyCollection<IReadOnlyCollection<(Node<TN, TE> Node, int Rank)>> DepthFirstSearch<TN, TE>(
+        IReadOnlyCollection<(Node<TN, TE> Node, int Rank)> currentPath,
+        Predicate<(Node<TN, TE> Node, int Rank)> targetPredicate)
+    {
+        var currentNode = currentPath.Last();
+        if (targetPredicate(currentNode))
+            return [currentPath];
+
+        var result = new List<IReadOnlyCollection<(Node<TN, TE> Node, int Rank)>>();
+        foreach (var edge in currentNode.Node.OutgoingEdges)
+        {
+            var nextNodeRank = currentNode.Rank + 1;
+            if (currentPath.Any(vn => vn.Node == edge.Target))
+                continue;
+            
+            result.AddRange(DepthFirstSearch(
+                currentPath.Concat([(edge.Target, nextNodeRank)]).ToList(), 
+                targetPredicate));
+        }
+
+        return result;
+    }
+}
--- 2024/Day10/Program.cs
+++ 2024/Day10/Program.cs
@@ -1,16 +1,70 @@
-﻿Console.WriteLine("Hello World!");
+﻿using Common.Graphs;
 
-record Node<TN, TE>(TN Value)
+using MyNode = Common.Graphs.Node<(int Height, int X, int Y), bool>;
+
+using var fh = File.OpenText("input1.txt");
+
+var nodes = new List<MyNode>();
+var previousLineNodes = new List<MyNode>();
+int y = -1;
+while (await fh.ReadLineAsync() is { } line)
 {
-    public List<Edge<TN, TE>> OutgoingEdges { get; } = [];
-    public List<Edge<TN, TE>> IncomingEdges { get; } = [];
-    
-    public void LinkTargetNode(TE value, Node<TN, TE> target)
+    y++;
+    var lineNodes = line
+        .Select(ch => (int)(ch - '0'))
+        .Select((height, x) => new MyNode((height, x, y)))
+        .ToList();
+    nodes.AddRange(lineNodes);
+
+    for (int x = 0; x < lineNodes.Count; x++)
     {
-        var e = new Edge<TN, TE>(value, this, target);
-        OutgoingEdges.Add(e);
-        target.IncomingEdges.Add(e);
+        if (x > 0 && ShouldBeLinked(lineNodes[x], lineNodes[x - 1]))
+            lineNodes[x].LinkTargetNode(true, lineNodes[x - 1]);
+        if (x < lineNodes.Count - 1 && ShouldBeLinked(lineNodes[x], lineNodes[x + 1]))
+            lineNodes[x].LinkTargetNode(true, lineNodes[x + 1]);
+        if (previousLineNodes.Count > x && ShouldBeLinked(lineNodes[x], previousLineNodes[x]))
+            lineNodes[x].LinkTargetNode(true, previousLineNodes[x]);
+        if (previousLineNodes.Count > x && ShouldBeLinked(previousLineNodes[x], lineNodes[x]))
+            previousLineNodes[x].LinkTargetNode(true, lineNodes[x]);
     }
+    previousLineNodes = lineNodes;
 }
 
-record Edge<TN, TE>(TE Value, Node<TN, TE> Source, Node<TN, TE> Target);
+{
+    Console.WriteLine("Part 1");
+
+    int sumScore = 0;
+    foreach (var trailHead in nodes.Where(n => n.Value.Height == 0))
+    {
+        var peaks = GraphSearch.BreadthFirstSearch(
+            trailHead, nt => nt.Node.Value.Height == 9);
+        sumScore += peaks.Count;
+    }
+    Console.WriteLine(sumScore);
+}
+
+{
+    Console.WriteLine("Part 2");
+
+    int sumScore = 0;
+    foreach (var trailHead in nodes.Where(n => n.Value.Height == 0))
+    {
+        var distinctPaths = GraphSearch.DepthFirstSearch(
+            trailHead, nt => nt.Node.Value.Height == 9);
+        sumScore += distinctPaths.Count;
+    }
+    Console.WriteLine(sumScore);
+}
+
+
+bool ShouldBeLinked(MyNode source, MyNode target)
+{
+    if (source.Value.Height + 1 != target.Value.Height) return false;
+
+    return (Math.Abs(source.Value.Y - target.Value.Y), Math.Abs(source.Value.X - target.Value.X)) switch
+    {
+        (0, 1) => true,
+        (1, 0) => true,
+        _ => false
+    };
+}
